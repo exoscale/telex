081bff0e49111d1c3abe6af819ef9ac091825ef4
Max Penet <max.penet@exoscale.ch>
2020-10-27 13:28:09 +0100

    add deploy repos

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "java.net.http helpers"
   :license {:name "ISC"}
   :url "https://github.com/exoscale/net-http"
+  :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [exoscale/interceptor "0.1.9"]
                  [exoscale/ex "0.3.15"]
