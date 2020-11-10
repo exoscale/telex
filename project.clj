86366a87974b39097f01cd49eaf9ad0239554add
Max Penet <max.penet@exoscale.ch>
2020-11-10 13:26:04 +0100

    Version 0.1.0-alpha5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/net-http "0.1.0-alpha5-SNAPSHOT"
+(defproject exoscale/net-http "0.1.0-alpha5"
   :description "java.net.http helpers"
   :license {:name "ISC"}
   :url "https://github.com/exoscale/net-http"
