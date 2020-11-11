fbd9fff5bb1d4b167eff81870b5b947b0bef8ef6
Max Penet <max.penet@exoscale.ch>
2020-11-11 14:38:51 +0100

    add enum specs

--- src/exoscale/net/http/client/enum.clj
+++ src/exoscale/net/http/client/enum.clj
@@ -4,4 +4,7 @@
                           HttpClient$Redirect)))
 
 (def version (enum/enum->fn HttpClient$Version))
+(enum/defspec :exoscale.net.http.client/version HttpClient$Version)
+
 (def redirect (enum/enum->fn HttpClient$Redirect))
+(enum/defspec :exoscale.net.http.client/redirect HttpClient$Redirect)
