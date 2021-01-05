d6bc1745027a8e0ac42f919bbbbecadb63e21b52
Max Penet <max.penet@exoscale.ch>
2021-01-05 09:56:38 +0100

    bump ex to 0.3.17

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [exoscale/interceptor "0.1.9"]
-                 [exoscale/ex "0.3.15"]
-                 [exoscale/ex-auspex "0.3.15"]
-                 [exoscale/ex-http "0.3.15"]
+                 [exoscale/ex "0.3.17"]
+                 [exoscale/ex-auspex "0.3.17"]
+                 [exoscale/ex-http "0.3.17"]
                  [cc.qbits/auspex "0.1.0-alpha4"]
                  [cc.qbits/commons "1.0.0-alpha3"]]
   :profiles {:dev  {:dependencies [[ring/ring-jetty-adapter    "1.7.1"]]}
