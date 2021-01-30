d4cd080e75621cb1ac1234c490f4141b676f56e6
Max Penet <max.penet@exoscale.ch>
2021-01-30 18:26:13 +0100

    less leaky ctx, prevent key duplicates

--- src/exoscale/telex/interceptor/ring1.clj
+++ src/exoscale/telex/interceptor/ring1.clj
@@ -6,61 +6,55 @@
   (:import (java.net.http HttpRequest
                           HttpResponse)))
 
+(def ring-keys [:url :body :method :query :headers :url
+                ;; exts
+                :form-params :query-params])
+
 (defn- ring1->http-request
   ^HttpRequest
-  [{:ring1.request/keys [url query method body headers]
-    :exoscale.telex.request/keys [timeout version expect-continue?]
-    :or {method :get}}]
+  [{{:keys [url query method body headers] :or {method :get}} :ring/request
+    :exoscale.telex.request/keys [timeout version expect-continue?]}]
   (request/http-request url query method body headers timeout version
                         expect-continue?))
 
 (defn- http-response->ring1
   [^HttpResponse http-response]
-  {:ring1.response/status (response/status http-response)
-   :ring1.response/body (response/body http-response)
-   :ring1.response/headers (response/headers->map http-response)})
+  {:status (response/status http-response)
+   :body (response/body http-response)
+   :headers (response/headers->map http-response)})
 
 (def ring-format-interceptor
   {:name ::map-format-interceptor
    :enter (fn [ctx]
-            (->> ctx
-                 (reduce-kv (fn [m k v]
-                              (assoc! m
-                                      (if (namespace k)
-                                        k
-                                        (keyword "ring1.request" (name k)))
-                                      v))
-                            (transient {}))
-                 (persistent!)))
+            (-> (apply dissoc ctx ring-keys)
+                (assoc :ring/request (select-keys ctx ring-keys))))
    :leave (fn [ctx]
-            (->> ctx
-                 (reduce-kv (fn [m k v]
-                              (cond-> m
-                                (= (namespace k) "ring1.response")
-                                (assoc! (keyword (name k)) v)))
-                            (transient ctx))
-                 (persistent!)))})
+            (-> ctx
+                (dissoc :ring/response :ring/request)
+                (conj (:ring/response ctx))))})
 
 (def request-interceptor
   {:name ::request
    :enter (fn [ctx]
             (assoc ctx :exoscale.telex/request (ring1->http-request ctx)))
    :leave (fn [ctx]
-            (into ctx (http-response->ring1 (:exoscale.telex/response ctx))))})
+            (assoc ctx
+                   :ring/response
+                   (http-response->ring1 (:exoscale.telex/response ctx))))})
 
 (def query-params-interceptor
   {:name ::query-params
    :enter
    (-> interceptor/encode-query-params
-       (ix/in [:ring1.request/query-params])
-       (ix/out [:ring1.request/query]))})
+       (ix/in [:ring/request :query-params])
+       (ix/out [:ring/request :query]))})
 
 (def form-params-interceptor
   {:name ::form-params
    :enter (-> interceptor/encode-query-params
-              (ix/in [:ring1.request/form-params])
-              (ix/out [:ring1.request/body])
-              (ix/when (fn [{:ring1.request/keys [form-params body]}]
+              (ix/in [:ring/request :form-params])
+              (ix/out [:ring/request :body])
+              (ix/when (fn [{{:keys [form-params body]} :ring/request}]
                          (and (not body)
                               (seq form-params)))))})
 
