97104fe3a4f7a91e826b81b8c2b1056c405bbda9
Max Penet <max.penet@exoscale.ch>
2021-01-31 17:07:02 +0100

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/telex "0.1.4-SNAPSHOT"
+(defproject exoscale/telex "0.1.4"
   :description "java.net.http helpers"
   :license {:name "ISC"}
   :url "https://github.com/exoscale/telex"
