c0ca012d0a99a9920c3219ae416dcf80f2c4017f
Max Penet <mpenet@s-exp.com>
2022-08-12 16:57:52 +0200

    mod release-tasks

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :license {:name "ISC"}
   :url "https://github.com/exoscale/telex"
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "--no-sign"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]]
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [exoscale/interceptor "0.1.12"]
                  [exoscale/ex "0.4.0"]
